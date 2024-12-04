12e0b29b2d24401c1505026b9ed6bc303e86ebb6
Adam Aske <adamaske@live.no>
2024-12-04 14:59:36 +0100

    DATA

--- conversion.py
+++ conversion.py
@@ -2,16 +2,53 @@ from mne.io import read_raw_snirf
 from mne_nirs.io import write_raw_snirf
 from mne.preprocessing.nirs import optical_density, beer_lambert_law
 import os
-subjects = ["subject01", "subject02", "subject03", "subject04"]
 
-for subject in subjects: #load raw snirf
-    path = os.path.join("data", subject, subject.join(".snirf"))
-    snirf = read_raw_snirf(path)
+def convert_all_files():
+    subject01_files = ["data/subject01/2024-12-04_001/2024-12-04_001.snirf",
+                   "data/subject01/2024-12-04_002/2024-12-04_002.snirf",
+                   "data/subject01/2024-12-04_003/2024-12-04_003.snirf"
+                   ]
+    subject02_files = ["data/subject02/2024-12-04_001/2024-12-04_004.snirf",
+                       "data/subject02/2024-12-04_002/2024-12-04_005.snirf",
+                       "data/subject02/2024-12-04_003/2024-12-04_006.snirf"
+                       ]
+    subject03_files = ["data/subject03/2024-12-04_001/2024-12-04_007.snirf",
+                       "data/subject03/2024-12-04_002/2024-12-04_008.snirf",
+                       "data/subject03/2024-12-04_003/2024-12-04_009.snirf"
+                       ]
+    subject04_files = ["data/subject04/2024-12-04_001/2024-12-04_010.snirf",
+                       "data/subject04/2024-12-04_002/2024-12-04_011.snirf",
+                       "data/subject04/2024-12-04_003/2024-12-04_012.snirf"
+                       ]
 
-    od = optical_density(snirf)
-    hb = beer_lambert_law(od)
+    new_files = []
 
-    new_path = os.path.join("data", subject.join("_hb.snirf"))
-    write_raw_snirf(hb, new_path)
+    subject_files = [subject01_files, subject02_files, subject03_files, subject04_files]
+    subjects = ["subject01"]
+
+    runs = ["run01", "run02", "run03", "run04", "run05", "run06", "run07", "run08", "run09"]
+
+    for s in range(len(subjects)):
+        subject = subjects[s]
+        files = subject_files[s]
+
+        for f in range(len(files)):
+
+            path = files[f]
+            snirf = read_raw_snirf(path)
+
+            od = optical_density(snirf)
+            hb = beer_lambert_law(od)
 
 
+            new_path = os.path.join("data", subject, subject + "_" + runs[f] + "_hb.snirf")
+            write_raw_snirf(hb, new_path)
+            new_files.append(new_path)
+
+    return new_files
+
+if __name__ == "__main__":
+    new_files = convert_all_files()
+    print("Converted to Hemoglobin : ")
+    for file in new_files:
+        print(file)
--- segmentation.py
+++ segmentation.py
@@ -1,14 +1,18 @@
 import os
 import numpy as np
 from mne.io import read_raw_snirf
+   
+filepaths = ["data/subject01/subject01_run01_hb.snirf",
+             "data/subject01/subject01_run02_hb.snirf",
+             "data/subject01/subject01_run03_hb.snirf",
+             ]       
 
-
-  
-subjects = ["subject01", "subject02", "subject03", "subject04"]
+right_data = []
+left_data = []
+rest_data = []
 data = []
-for subject in subjects: #load raw snirf
-    path = os.path.join("data", subject, subject.join("_hb.snirf"))
-    snirf = read_raw_snirf(path)
+for filepath in filepaths: #load raw snirf
+    snirf = read_raw_snirf(filepath)
     
         
     snirf_data = np.array(snirf.get_data())
@@ -18,28 +22,40 @@ for subject in subjects: #load raw snirf
     onsets = snirf.annotations.onset
     tasks = snirf.annotations.description
     durations = snirf.annotations.duration
-    labels = {"rest":0, "right" : 1, "left": 2}
+    print("File : ", filepath)
+
+    print("onsets : ", onsets)
+    print("tasks : ", tasks)
+    print("durations : ", durations)
+    labels = {"rest":0, "right" : 1, "left": 2, "end":3}
     
     current_time = 0
     
-    for i in range(len(onsets)):
+    for i in range(len(onsets)): #for every marker
         onset = onsets[i]
         task = tasks[i]
         label = labels[task]
         duration = durations[i]
         
-        start_time = onset
+        if(task == 3): # skip end data
+            continue
+
+        start_time = onset #
         end_time = start_time + duration
         
-        start_pos = int(start_time * sfreq)
+        start_pos = int(start_time * sfreq) #seconds into frame
         end_pos = int(end_time * sfreq)
         
-        #grab everything between current_time and start_time as a "rest"
+        task_data = snirf_data[:, start_pos:end_pos] #get all channels in range
         
-        current_time = end_time
-        
-        
+        if label == "rest":
+            rest_data.append(task_data)
+        if label == "right":
+            right_data.append(task_data)
+        if label == "left":
+            left_data.append(task_data)
+    
 
-    data.append(data)
-
-
+right_data = np.array(rest_data)
+left_data = np.array(right_data)
+rest_data = np.array(left_data)
